36bca00b0f4ba87e0e834c7ae674c437638c6186
MiBeloz <miha_bmv_@mail.ru>
2024-01-29 23:50:36 +0600

    Initial

---
+++ Source/CppBase/Public/CppBaseActor.h
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CppBaseActor.generated.h"
+
+UENUM(BlueprintType)
+enum class EMovementState : uint8 {
+	Mobility,
+	Static
+};
+
+UCLASS()
+class CPPBASE_API ACppBaseActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACppBaseActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere)
+	FString InstanceName = UObject::GetName();
+
+	UPROPERTY(EditInstanceOnly)
+	int EnemyNum = 0;
+
+	UPROPERTY(EditInstanceOnly)
+	bool isAlive = false;
+
+	UFUNCTION(BlueprintCallable)
+	void ShowActorInformation();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
+
+USTRUCT(BlueprintType)
+struct FTransformStruct {
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector CurrentLocation = FVector::ZeroVector;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FRotator CurrentRotator = FRotator::ZeroRotator;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector CurrentScale = FVector(1.0f, 1.0f, 1.0f);
+};
+
